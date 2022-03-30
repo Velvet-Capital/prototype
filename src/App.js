c091b5eabdce26b278d307692db6ecad89d393a4
langnavina97 <81016364+langnavina97@users.noreply.github.com>
2022-03-30 15:32:25 -0300

    added some more tokens

--- src/App.js
+++ src/App.js
@@ -30,9 +30,18 @@ class App extends Component {
 
       nftTokenBalance: 0,
       defiTokenBalance: 0,
-      usdtTokenBalance: 0,
-      busdTokenBalance: 0,
-      daiTokenBalance: 0,
+
+      btcTokenBalance: 0,
+      ethTokenBalance: 0,
+      shibaTokenBalance: 0,
+      xrpTokenBalance: 0,
+      ltcTokenBalance: 0,
+
+      axsTokenBalance: 0,
+      manaTokenBalance: 0,
+      sandTokenBalance: 0,
+      thetaTokenBalance: 0,
+      flowTokenBalance: 0,
 
       rate: 0
     }
@@ -63,9 +72,9 @@ class App extends Component {
     const web3 = window.web3;
     const accounts = await window.web3.eth.getAccounts();
     this.setState({ account: accounts[0] })
-    const SwapContract = new web3.eth.Contract(IndexSwap.abi, "0x38F6EDad57B87e412f4AdF0119d4f735E46DD802");
-    const NFTTokenContract = new web3.eth.Contract(IndexToken.abi, "0x64a9057A0DbddDfB8A81b69ccf7536E4bAde0AbE");
-    const DeFiTokenContract = new web3.eth.Contract(IndexToken.abi, "0xFd22F799B90888973dc841a11E6ED0cf24f0b7e4");
+    const SwapContract = new web3.eth.Contract(IndexSwap.abi, "0xc15B79570f727A334fA13de8801cCF6B1BA16973");
+    const NFTTokenContract = new web3.eth.Contract(IndexToken.abi, "0xE870b73661Cc3De504FE26111748c08224EDBf63");
+    const DeFiTokenContract = new web3.eth.Contract(IndexToken.abi, "0xAe24BD25B1Aba33f69e97074aF954b1BF84B72Cb");
     this.setState({ SwapContract, NFTTokenContract, DeFiTokenContract });
   }
 
@@ -110,7 +119,7 @@ class App extends Component {
     const web3 = window.web3;
     const v = this.state.defiToMint;
     const amount = web3.utils.toWei(v, "ether");
-    const resp = await this.state.SwapContract.methods.investInFundDeFi().send({from: this.state.account, value: amount});
+    const resp = await this.state.SwapContract.methods.investInFundDefi().send({from: this.state.account, value: amount});
     if(resp.status) {
       swal("Investment successfull!", `You invested ${v} BNB into the DeFi Portfolio.`, "success");
 
@@ -130,19 +139,48 @@ class App extends Component {
     const defiTokenBalanceRes = await this.state.DeFiTokenContract.methods.balanceOf(this.state.account).call();
     const defiTokenBalance = web3.utils.fromWei(defiTokenBalanceRes, "ether");
 
-    const USDTokenConntract = new web3.eth.Contract(IERC.abi, "0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684");
-    const usdtTokenBalanceRes = await USDTokenConntract.methods.balanceOf(this.state.account).call();
-    const usdtTokenBalance = web3.utils.fromWei(usdtTokenBalanceRes, "ether");
+    const BTCTokenConntract = new web3.eth.Contract(IERC.abi, "0x419816F160C9bf1Bb8F297d17E1bF44207B9E06C");
+    const btcTokenBalanceRes = await BTCTokenConntract.methods.balanceOf(this.state.account).call();
+    const btcTokenBalance = web3.utils.fromWei(btcTokenBalanceRes, "ether");
 
-    const BUSDTokenConntract = new web3.eth.Contract(IERC.abi, "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7");
-    const busdTokenBalanceRes = await BUSDTokenConntract.methods.balanceOf(this.state.account).call();
-    const busdTokenBalance = web3.utils.fromWei(busdTokenBalanceRes, "ether");
+    const ETHTokenConntract = new web3.eth.Contract(IERC.abi, "0x8BaBbB98678facC7342735486C851ABD7A0d17Ca");
+    const ethTokenBalanceRes = await ETHTokenConntract.methods.balanceOf(this.state.account).call();
+    const ethTokenBalance = web3.utils.fromWei(ethTokenBalanceRes, "ether");
 
-    const DAITokenConntract = new web3.eth.Contract(IERC.abi, "0x8a9424745056Eb399FD19a0EC26A14316684e274");
-    const daiTokenBalanceRes = await DAITokenConntract.methods.balanceOf(this.state.account).call();
-    const daiTokenBalance = web3.utils.fromWei(daiTokenBalanceRes, "ether");
+    const SHIBATokenConntract = new web3.eth.Contract(IERC.abi, "0xb950614981c8bc650744347293e3AC4Ea8ff123B");
+    const shibaTokenBalanceRes = await SHIBATokenConntract.methods.balanceOf(this.state.account).call();
+    const shibaTokenBalance = web3.utils.fromWei(shibaTokenBalanceRes, "ether");
+
+    const XRPTokenConntract = new web3.eth.Contract(IERC.abi, "0x53E5D0c3702Fb163C8650ee74697BE9beF38b9E5");
+    const xrpTokenBalanceRes = await XRPTokenConntract.methods.balanceOf(this.state.account).call();
+    const xrpTokenBalance = web3.utils.fromWei(xrpTokenBalanceRes, "ether");
+
+    const LTCTokenConntract = new web3.eth.Contract(IERC.abi, "0xC3c89a5Cd270BFB4f948CEF2151d818E3cB9bDD2");
+    const ltcTokenBalanceRes = await LTCTokenConntract.methods.balanceOf(this.state.account).call();
+    const ltcTokenBalance = web3.utils.fromWei(ltcTokenBalanceRes, "ether");
+
+    const AXSTokenConntract = new web3.eth.Contract(IERC.abi, "0xf1955902806EE5b144691a3de9005951079d8458");
+    const axsTokenBalanceRes = await AXSTokenConntract.methods.balanceOf(this.state.account).call();
+    const axsTokenBalance = web3.utils.fromWei(axsTokenBalanceRes, "ether");
+
+    const MANATokenConntract = new web3.eth.Contract(IERC.abi, "0xF5D59a1A7667c23EA6b369085b8C693bE6A6af5C");
+    const manaTokenBalanceRes = await MANATokenConntract.methods.balanceOf(this.state.account).call();
+    const manaTokenBalance = web3.utils.fromWei(manaTokenBalanceRes, "ether");
+
+    const SANDTokenConntract = new web3.eth.Contract(IERC.abi, "0x54FA926a8C1241174aFd79F3F1Ea3219D11Afb7E");
+    const sandTokenBalanceRes = await SANDTokenConntract.methods.balanceOf(this.state.account).call();
+    const sandTokenBalance = web3.utils.fromWei(sandTokenBalanceRes, "ether");
+
+    const THETATokenConntract = new web3.eth.Contract(IERC.abi, "0x2f6131AF2dcb1315BA63bCcd0a5E3a236A5C5c93");
+    const thetaTokenBalanceRes = await THETATokenConntract.methods.balanceOf(this.state.account).call();
+    const thetaTokenBalance = web3.utils.fromWei(thetaTokenBalanceRes, "ether");
+
+    const FLOWTokenConntract = new web3.eth.Contract(IERC.abi, "0x190bB213CF401A4E5d8820b12198E92A858239C6");
+    const flowTokenBalanceRes = await FLOWTokenConntract.methods.balanceOf(this.state.account).call();
+    const flowTokenBalance = web3.utils.fromWei(flowTokenBalanceRes, "ether");
     
-    this.setState({ nftTokenBalance, defiTokenBalance, usdtTokenBalance, busdTokenBalance,daiTokenBalance });
+    this.setState({ nftTokenBalance, defiTokenBalance, btcTokenBalance, ethTokenBalance, shibaTokenBalance, xrpTokenBalance,
+                    ltcTokenBalance, axsTokenBalance, manaTokenBalance, sandTokenBalance, thetaTokenBalance, flowTokenBalance });
   }
 
   getRate = async() => {
@@ -172,9 +210,7 @@ class App extends Component {
                     <Card.Header style={{color: "white"}}>Top 5 DeFi Tokens</Card.Header>
                     <Card.Description>
 
-                    <p style={{color: "#C0C0C0"}}>Rate: In return of investing 1 BNB you will receive {this.state.rate} DeFi Token.
-
-</p>
+                    <p style={{color: "#C0C0C0"}}>Rate: In return of investing 1 BNB you will receive {this.state.rate} DeFi Token.</p>
 
                       <Form onSubmit={this.investDeFi}>
                         <Input style={{ width: "300px", padding: 3 }} required type="text" placeholder="BNB amount to invest" name="defiToMint" onChange={this.handleInputChange}></Input>
@@ -200,18 +236,25 @@ class App extends Component {
                             <Table.Cell style={{color: "#C0C0C0"}}>{this.state.defiTokenBalance}</Table.Cell>
                           </Table.Row>
                           <Table.Row>
-                            <Table.Cell style={{color: "#C0C0C0"}}>USDT</Table.Cell>
-                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.usdtTokenBalance}</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Bitcoin (BTC)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.btcTokenBalance}</Table.Cell>
                           </Table.Row>
                           <Table.Row>
-                            <Table.Cell style={{color: "#C0C0C0"}}>BUSD</Table.Cell>
-                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.busdTokenBalance}</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Ethereum (ETH)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.ethTokenBalance}</Table.Cell>
                           </Table.Row>
                           <Table.Row>
-                            <Table.Cell style={{color: "#C0C0C0"}}>DAI</Table.Cell>
-                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.daiTokenBalance}</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Shiba Ibu (SHIB)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.shibaTokenBalance}</Table.Cell>
                           </Table.Row>
-                          
+                          <Table.Row>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Ripple (XRP)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.xrpTokenBalance}</Table.Cell>
+                          </Table.Row>
+                          <Table.Row>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Litecoin (LTC)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.ltcTokenBalance}</Table.Cell>
+                          </Table.Row> 
                         </Table.Body>
                       </Table>
             
@@ -255,16 +298,24 @@ class App extends Component {
                             <Table.Cell style={{color: "#C0C0C0"}}>{this.state.nftTokenBalance}</Table.Cell>
                           </Table.Row>
                           <Table.Row>
-                            <Table.Cell style={{color: "#C0C0C0"}}>USDT</Table.Cell>
-                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.usdtTokenBalance}</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Axie Infinity (AXS)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.axsTokenBalance}</Table.Cell>
                           </Table.Row>
                           <Table.Row>
-                            <Table.Cell style={{color: "#C0C0C0"}}>BUSD</Table.Cell>
-                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.busdTokenBalance}</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Decentraland (MANA)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.manaTokenBalance}</Table.Cell>
                           </Table.Row>
                           <Table.Row>
-                            <Table.Cell style={{color: "#C0C0C0"}}>DAI</Table.Cell>
-                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.daiTokenBalance}</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>The Sandbox (SAND)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.sandTokenBalance}</Table.Cell>
+                          </Table.Row>
+                          <Table.Row>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Theta Network (THETA)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.thetaTokenBalance}</Table.Cell>
+                          </Table.Row>
+                          <Table.Row>
+                            <Table.Cell style={{color: "#C0C0C0"}}>Flow (FLOW)</Table.Cell>
+                            <Table.Cell style={{color: "#C0C0C0"}}>{this.state.flowTokenBalance}</Table.Cell>
                           </Table.Row>
                           
                         </Table.Body>
